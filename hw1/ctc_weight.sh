bd8126e9fcdf48242b7fc7a33e7d80a066e2922c
ming024 <b04901102@ntu.edu.tw>
2020-03-18 22:36:53 +0800

    final submit

--- hw1/ctc_weight.sh
+++
@@ -1,4 +0,0 @@
-python3 main.py --config config/dlhlp/asr_ctc_0.3.yaml
-python3 main.py --config config/dlhlp/asr_ctc_0.4.yaml
-python3 main.py --config config/dlhlp/asr_ctc_0.6.yaml
-python3 main.py --config config/dlhlp/asr_ctc_0.7.yaml
