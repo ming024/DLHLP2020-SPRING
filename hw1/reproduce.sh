1dd48607adb537a8f9ee81af6e5bd8543807b5b8
ming024 <b04901102@ntu.edu.tw>
2020-03-18 22:32:10 +0800

    final submit

--- hw1/reproduce.sh
+++ hw1/reproduce.sh
@@ -3,6 +3,7 @@ python3 gdrive_downloader.py 1L5VR-Bsu4BCRTsXyhrjUE7ni1WJWvnr- best_att.pth
 sed -i "s@data_dir@$1@g" config/dlhlp/asr.yaml 
 sed -i "s@data_dir@$1@g" config/dlhlp/lm.yaml 
 sed -i "s@ckpt/asr_sd0/@@g" config/dlhlp/decode.yaml 
+sed -i "s@ckpt/lm_sd0/@@g" config/dlhlp/decode.yaml 
 python3 main.py --config config/dlhlp/decode.yaml --test --njobs 8
 python3 format.py result/decode_test_output.csv result/kaggle.csv
 mv result/kaggle.csv $2
