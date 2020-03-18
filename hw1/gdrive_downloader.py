22795479d94eaf3809c50744618c305897291ca6
ming024 <b04901102@ntu.edu.tw>
2020-03-18 20:33:42 +0800

    submit

---
+++ hw1/gdrive_downloader.py
@@ -0,0 +1,40 @@
+import requests
+import sys
+
+def download_file_from_google_drive(id, destination):
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination)    
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+
+if __name__ == "__main__":
+    #var = input("Please enter public file id : ")
+    var = sys.argv[1]
+    file_id = str(var)
+    #name = input("Please enter destination : ")
+    name = sys.argv[2]
+    destination = str(name)
+    download_file_from_google_drive(file_id, destination)
